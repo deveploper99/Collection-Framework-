e895e9ea0bf5c0e3e819e721b9bd1c2c8c516af6
deveploper99 <rtxdeveploper99@gmail.com>
2026-02-23 12:40:15 +0600

    create arrayList operation methode practis

--- src/main/java/org/rtxrassel/Main.java
+++ src/main/java/org/rtxrassel/Main.java
@@ -4,36 +4,60 @@ import java.util.ArrayList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        ArrayList<String> student = new ArrayList<>();
-        student.add("Rassel Hassan");
-        student.add("Siam ahmed");
-        student.add("taslim");
-        student.add(1,"Karimul Islam"); // add at index
-        student.add(0,"Siam");
-        student.set(0,"======="); // replace
-        student.removeFirst(); //1st object remove
-        student.removeLast(); //last object delete
+    public static void main(String[] args) {
+//        ArrayList<String> student = new ArrayList<>();
+//        student.add("Rassel Hassan");
+//        student.add("Siam ahmed");
+//        student.add("taslim");
+//        student.add(1,"Karimul Islam"); // add at index
+//        student.add(0,"Siam");
+//        student.set(0,"======="); // replace
+//        student.removeFirst(); //1st object remove
+//        student.removeLast(); //last object delete
+//
+//        student.clear();
+//        if(student.contains("Siam ahmed")){
+//            System.out.println("Yes");
+//        }else{                              // contains diye khuja
+//            System.out.println("no");
+//
+//        }
+//
+//        System.out.println("ArrayList er Size:"+student.size());
+//        System.out.println("ArrayList is Emty: " +student.isEmpty());
+//
+//
+//
+//
+//
+//        for(String s:student){
+//            System.out.println(s);
+//        }
 
-        student.clear();
-        if(student.contains("Siam ahmed")){
-            System.out.println("Yes");
-        }else{                              // contains diye khuja
-            System.out.println("no");
 
-        }
-
-        System.out.println("ArrayList er Size:"+student.size());
-        System.out.println("ArrayList is Emty: " +student.isEmpty());
-
-
-
-
-
-        for(String s:student){
-            System.out.println(s);
-        }
+        arraylistIterator();
 
 
     }
-}
+
+    private static void arraylistIterator() {
+        ArrayList<String> names = new ArrayList<>();
+            names.add("Rassel Hassan");
+            names.add("Rakib");
+            names.add("Akramul");
+
+            for (int i=0; i<names.size();i++){
+
+                if(names.get(i).equalsIgnoreCase("Rakib")){
+                    break;
+                }
+
+                System.out.println(names.get(i).toLowerCase());
+
+            }
+        }
+    }
+
+
+
+
