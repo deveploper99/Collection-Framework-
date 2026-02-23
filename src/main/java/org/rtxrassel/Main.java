5a07932030352ff0d64d16d29f3c734f10ec84f5
deveploper99 <rtxdeveploper99@gmail.com>
2026-02-23 08:46:53 +0600

    create arrayList operation methode practis

--- src/main/java/org/rtxrassel/Main.java
+++ src/main/java/org/rtxrassel/Main.java
@@ -9,6 +9,26 @@ public class Main {
         student.add("Rassel Hassan");
         student.add("Siam ahmed");
         student.add("taslim");
+        student.add(1,"Karimul Islam"); // add at index
+        student.add(0,"Siam");
+        student.set(0,"======="); // replace
+        student.removeFirst(); //1st object remove
+        student.removeLast(); //last object delete
+
+        student.clear();
+        if(student.contains("Siam ahmed")){
+            System.out.println("Yes");
+        }else{                              // contains diye khuja
+            System.out.println("no");
+
+        }
+
+        System.out.println("ArrayList er Size:"+student.size());
+        System.out.println("ArrayList is Emty: " +student.isEmpty());
+
+
+
+
 
         for(String s:student){
             System.out.println(s);
