ccb6c51a6eee486b6bae52dd7eba0c96ee7fe228
deveploper99 <rtxdeveploper99@gmail.com>
2026-02-23 07:57:00 +0600

    create arrayList

--- src/main/java/org/rtxrassel/Main.java
+++ src/main/java/org/rtxrassel/Main.java
@@ -1,21 +1,19 @@
 package org.rtxrassel;
-
 import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        ArrayList<String> students = new ArrayList<>();
+        ArrayList<String> student = new ArrayList<>();
+        student.add("Rassel Hassan");
+        student.add("Siam ahmed");
+        student.add("taslim");
 
-        students.add("Rassel");
-        students.add("Karim");
-        students.add("Rahim");
-
-        System.out.println("Total Student: " + students.size());
-
-        for(String s : students){
+        for(String s:student){
             System.out.println(s);
         }
+
+
     }
 }
